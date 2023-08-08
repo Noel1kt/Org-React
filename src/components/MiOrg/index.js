de66082e17b7170b3a041643d611a8b365e00adf
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-08-08 12:27:31 -0600

    hooks

--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,21 @@
 import "./MiOrg.css"
+import { useState } from "react"
 
 const MiOrg = () => {
+    //Estado - hooks
+    //useState
+    //useState()
+
+    //const [nombre,actulaizarNombre] = useState("Noel")
+    const [mostrar, actualizarMostrar] = useState(true)
+    const manejarClick = () => { 
+        console.log("Mostrar elemento", !mostrar);
+        actualizarMostrar(!mostrar);
+    }
+
     return <section className="orgSection">
         <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add"/>
+        <img src="/img/add.png" alt="add" onClick={manejarClick}/>
     </section>
 }
 
